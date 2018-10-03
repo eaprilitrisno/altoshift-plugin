8283939fcb6d962ddb9a47d1a47bee4cb03d06f2
EKO APRILI TRISNO <ekoaprilitrisno17@gmail.com>
2018-10-03 12:52:57 +0700

    hello

---
+++ altoshift-plugin.php
@@ -0,0 +1,15 @@
+<?php
+/**
+*
+* @package AltoshiftPlugin
+*/
+/*
+Plugin Name: Altoshift Plugin
+Plugin URI: https://altoshift.com/plugin
+Description: Learning Only Project.
+Version: 1.0.0
+Author: Eko Aprili Trisno "eatrisno"
+Author URI: eatrisno.id
+License: GPLv2 or later
+Text Domain: altoshift-plugin
+*/
